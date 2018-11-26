cafcb582625ae10c7038cc066cb30234ad3a8d27
zlotyy <arek.gajewski.123@gmail.com>
2018-11-26 16:02:41 +0100

    emit - passing data into parent component

--- src/build/RobotBuilder.vue
+++ src/build/RobotBuilder.vue
@@ -1,8 +1,23 @@
 <template>
   <div class="content">
-    <button class="add-to-cart" @click="addToCart()">
+    <div class="preview">
+      <div class="preview-content">
+        <div class="top-row">
+          <img :src="selectedRobot.head.src"/>
+        </div>
+        <div class="middle-row">
+          <img :src="selectedRobot.leftArm.src" class="rotate-left"/>
+          <img :src="selectedRobot.torso.src"/>
+          <img :src="selectedRobot.rightArm.src" class="rotate-right"/>
+        </div>
+        <div class="bottom-row">
+          <img :src="selectedRobot.base.src"/>
+        </div>
+      </div>
+      <button class="add-to-cart" @click="addToCart()">
       Add to Cart
     </button>
+    </div>
     <div class="top-row">
       <!-- <div class="robot-name">
         {{selectedRobot.head.title}}
@@ -10,23 +25,28 @@
       </div> -->
       <PartSelector
       :parts="availableParts.heads"
-      position="top" />
+      position="top"
+      @partSelected="part => selectedRobot.head=part" />
     </div>
     <div class="middle-row">
       <PartSelector
       :parts="availableParts.arms"
-      position="left" />
+      position="left"
+      @partSelected="part => selectedRobot.leftArm=part" />
       <PartSelector
       :parts="availableParts.torsos"
-      position="center" />
+      position="center"
+      @partSelected="part => selectedRobot.torso=part" />
       <PartSelector
       :parts="availableParts.arms"
-      position="right" />
+      position="right"
+      @partSelected="part => selectedRobot.rightArm=part" />
     </div>
     <div class="bottom-row">
       <PartSelector
       :parts="availableParts.bases"
-      position="bottom" />
+      position="bottom"
+      @partSelected="part => selectedRobot.base=part" />
     </div>
     <div>
       <h1>Cart</h1>
@@ -199,8 +219,7 @@ export default {
   }
   .add-to-cart {
     position: absolute;
-    right: 30px;
-    width: 220px;
+    width: 210px;
     padding: 3px;
     font-size: 16px;
   }
@@ -214,5 +233,26 @@ export default {
   }
   .sale-border {
     border: 3px solid red;
+  }
+  .preview {
+  position: absolute;
+  top: -20px;
+  right: 0;
+  width: 210px;
+  height: 210px;
+  padding: 5px;
+  }
+  .preview-content {
+    border: 1px solid #999;
+  }
+  .preview img {
+    width: 50px;
+    height: 50px;
+  }
+  .rotate-right {
+    transform: rotate(90deg);
+  }
+  .rotate-left {
+    transform: rotate(-90deg);
   }
 </style>
