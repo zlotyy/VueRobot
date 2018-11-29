538fd85d40945b6884ea33f6070b2520e5f9c51a
zlotyy <arek.gajewski.123@gmail.com>
2018-11-29 16:14:55 +0100

    preventing navigation, preventing pages from loading, enabling html5 history mode, using named views

--- src/build/RobotBuilder.vue
+++ src/build/RobotBuilder.vue
@@ -78,10 +78,21 @@ import CollapsibleSection from '../shared/CollapsibleSection.vue';
 
 export default {
   name: 'RobotBuilder',
+  beforeRouteLeave(to, from, next) {
+    if (this.addedToCart) {
+      next(true);
+    } else {
+      /* eslint no-alert: 0 */
+      /* eslint no-restricted-globals: 0 */
+      const response = confirm('You have not added your robot to your cart, are you sure you want to leave?');
+      next(response);
+    }
+  },
   components: { PartSelector, CollapsibleSection },
   data() {
     return {
       availableParts,
+      addedToCart: false,
       cart: [],
       selectedRobot: {
         head: {},
@@ -112,6 +123,7 @@ export default {
         + robot.torso.cost
         + robot.base.cost;
       this.cart.push(Object.assign({}, robot, { cost }));
+      this.addedToCart = true;
     },
   },
 };
